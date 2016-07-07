2b4778fd7f5f950544d94ae6f3dd6e0bd8c515ed
Mendez Cano <am47121@nsroot.net>
2016-07-06 19:50:47 -0500

    Fixing issues with previous commit

--- app/shared/constants.js
+++ app/shared/constants.js
@@ -1,13 +1,5 @@
 angular.module('constants',[]).constant('FLICKR',{
-<<<<<<< HEAD
 	api_key : "e06443c1e6342ce6b0c4f2d0247af627",
-=======
-<<<<<<< HEAD
-	api_key : "f5ec3d75757714e5dddb683f05772666",
-=======
-	api_key : "e06443c1e6342ce6b0c4f2d0247af627",
->>>>>>> 9d5dfa9f25b29743bc37797b57097c976ab2e887
->>>>>>> 2b3beec333c9e6c0d9dffcd206a25a5a10d95354
 	format : "json",
 	nojsoncallback : 1
 });
