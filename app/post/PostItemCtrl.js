c60280c70c3132be44b6b41b583f2f2314148c08
Mendez Cano <am47121@nsroot.net>
2016-07-12 17:55:15 -0500

    Adding form validation to edit post

--- app/post/PostItemCtrl.js
+++ app/post/PostItemCtrl.js
@@ -2,11 +2,15 @@ angular.module('application').controller('PostItemCtrl',
 		['$scope','$log', '$state', '$stateParams', 'Posts', function($scope, $log, $state, $stateParams, Posts) {
 			$log.info("Executing controller: PostItemCtrl");
 			$scope.post = Posts.get({id: $stateParams.id});
-			$scope.save=function(){
-				$scope.post.$update(function(){
-					alert("The post has been updated");
-					$state.go('^');
-				});
+			$scope.save=function(isValid){
+				if(isValid){
+					$scope.post.$update(function(){
+						alert("The post has been updated");
+						$state.go('^');
+					});
+				}
+				else
+					alert("Form is invalid"); //test this adding comments to submit button
 			};
 			$scope.cancel = function(){
 				$log.info("Return to posts list");
