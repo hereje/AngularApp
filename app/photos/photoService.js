1327a41da461e410737ebf611d8437d74e6a9f62
Angel Mendez <amendez1988@gmail.com>
2016-07-15 00:38:38 -0500

    Adding a better error handling for Flickr API

--- app/photos/photoService.js
+++ app/photos/photoService.js
@@ -2,6 +2,15 @@
 	"use strict";
 angular.module('application').service('photoService',['$http', '$q', 'FLICKR' ,photoService]);
 function photoService($http, $q, FLICKR) {
+	function getFailure(res){
+		if(res.data.stat){ //Handling Flickr API errors
+			if(res.data.stat == "fail"){
+				alert(JSON.stringify(res.data.message));
+				return $q.reject(res);
+			}
+		}
+		return res;
+	}
 	return {
 		search : function (tags){
 			//TODO: Inject general values
@@ -11,8 +20,7 @@ function photoService($http, $q, FLICKR) {
 			function success(response){
 				//prevent get non json objects
 				if (typeof response.data === 'object') {
-					// successful response
-					return response;
+					return getFailure(response);
 				} else {
 					// invalid response
 					return $q.reject(response);
@@ -31,8 +39,7 @@ function photoService($http, $q, FLICKR) {
 	    		function success(response) {
 					//prevent get non json objects
 	    			if (typeof response.data === 'object') {
-	    				// successful response
-	    				return response;
+	    				return getFailure(response);
 	    			} else {
 	    				// invalid response
 	    				return $q.reject(response);
--- app/shared/interceptors.js
+++ app/shared/interceptors.js
@@ -1,44 +1,39 @@
-var app = angular.module('interceptors', []);
+(function () {
+	'use strict';
+	angular.module('interceptors', []).factory('requestsInterceptor',['$log','$q', requestsInterceptor]);
+	angular.module('interceptors').config(
+		function ($httpProvider) {
+		$httpProvider.interceptors.push('requestsInterceptor');
+	});
 
-// Intercept all requests and responses
-function requestsInterceptor($log, $q) {
-	return {
-		request : function(request) {
-			// Set global timeout, value given in milliseconds
-			request.timeout = 2*60*1000; //2 minutes
-			return request;
-		},
+	// Intercept all requests and responses
+	function requestsInterceptor($log, $q) {
+		return {
+			request : function (request) {
+				// Set global timeout, value given in milliseconds
+				request.timeout = 2 * 60 * 1000; //2 minutes
+				return request;
+			},
 
-		requestError : function(request) {
-			$log.warn("Request Error", request);
-			return $q.reject(request); //reject promise
-		},
+			requestError : function (request) {
+				$log.warn("Request Error", request);
+				return $q.reject(request); //reject promise
+			},
 
-		response : function(res) {
-			if(res.data.stat){ //Handling Flickr API errors
-				if(res.data.stat == "fail"){
-					alert(JSON.stringify(res.data.message));
-					return $q.reject(res);
+			response : function (res) {
+				return res;
+			},
+
+			responseError : function (res) {
+				//handling http errors
+				if (typeof res.data === 'object') {
+					$log.warn(res);
+					alert(JSON.stringify(res));
+				} else {
+					alert(res);
 				}
+				return $q.reject(res);
 			}
-			return res;
-		},
-
-		responseError : function(res) {
-			//handling http errors
-			if (typeof res.data === 'object') {
-				$log.warn(res);
-				alert(JSON.stringify(res));
-			}
-			else{
-				alert(res);
-			}
-			return $q.reject(res);
-		}
+		};
 	}
-}
-
-app.factory('requestsInterceptor', requestsInterceptor).config(
-		function($httpProvider) {
-			$httpProvider.interceptors.push('requestsInterceptor');
-		});
+})();
